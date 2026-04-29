76dfd4937d342e72b6e71f68ca057cf55564d37b
jadaughtrey-cell <jadaughtrey@gmail.com>
2026-04-28 22:41:39 -0400

    update nav: Watch Demo link, Fetch Canvas Metadata button, YouTube link

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,4 @@
-"use client";
+↗"use client";
 import { useState, useEffect, useRef } from "react";
 import SetupForm from "@/components/SetupForm";
 import AssignmentList from "@/components/AssignmentList";
